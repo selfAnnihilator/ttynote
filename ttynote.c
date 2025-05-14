12f64fc5938378c299f16d4f324a33308d4da0fd
selfAnnihilator <159141717+selfAnnihilator@users.noreply.github.com>
2025-05-14 08:13:18 +0530

    changes in cursor movement in line

--- ttynote.c
+++ ttynote.c
@@ -304,6 +304,9 @@ void editorMoveCursor(int key) {
         case ARROW_LEFT:
             if(E.cx != 0) {
                 E.cx--;
+            } else if(E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_RIGHT:
@@ -321,6 +324,12 @@ void editorMoveCursor(int key) {
                 E.cy++;
             }   
             break;
+    }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
     }
 }
 
