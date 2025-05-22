11d3a81e46ca704ab3414f4a720afcbb7ace4b09
selfAnnihilator <159141717+selfAnnihilator@users.noreply.github.com>
2025-05-22 21:37:48 +0530

    added filetype detection for c files

--- ttynote.c
+++ ttynote.c
@@ -45,8 +45,16 @@ enum editorHighlight {
     HL_MATCH
 };
 
+#define HL_HIGHLIGHT_NUMBERS (1<<0)
+
 
 /**** data ****/
+
+struct editorSyntax {
+    char *filetype;
+    char **filematch;
+    int flags;
+};
 
 typedef struct erow {
     int size;
@@ -69,10 +77,26 @@ struct editorConfig {
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
+    struct editorSyntax *syntax;
     struct termios origTermios;
 };
 
 struct editorConfig E;
+
+
+/**** filetypes ****/
+
+char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
+
+struct editorSyntax HLDB[] = {
+    {
+        "c",
+        C_HL_extensions,
+        HL_HIGHLIGHT_NUMBERS
+    },
+};
+
+#define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
 
 
 /**** prototypes ****/
@@ -204,6 +228,8 @@ void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
+    if(E.syntax == NULL) return;
+
     int prevSep = 1;
 
     int i = 0;
@@ -211,11 +237,13 @@ void editorUpdateSyntax(erow *row) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-        if((isdigit(c) && (prevSep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
-            row->hl[i] = HL_NUMBER;
-            i++;
-            prevSep = 0;
-            continue;
+        if(E.syntax->flags && HL_HIGHLIGHT_NUMBERS) {
+            if((isdigit(c) && (prevSep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
+                row->hl[i] = HL_NUMBER;
+                i++;
+                prevSep = 0;
+                continue;
+            }
         }
 
         prevSep = isSeperator(c);
@@ -228,6 +256,31 @@ int editorSyntaxToColor(int hl) {
         case HL_NUMBER: return 31;
         case HL_MATCH: return 34;
         default: return 37;
+    }
+}
+
+void editorSelectSyntaxHighlight() {
+    E.syntax = NULL;
+    if(E.filename == NULL) return;
+
+    char *ext = strrchr(E.filename, '.');
+
+    for(unsigned int j = 0; j < HLDB_ENTRIES; j++) {
+        struct editorSyntax *s = &HLDB[j];
+        unsigned int i = 0;
+        while(s->filematch[i]) {
+            int is_ext = (s->filematch[i][0] == '.');
+            if((is_ext && ext && !strcmp(ext, s->filematch[i])) || (!is_ext && strstr(E.filename, s->filematch[i]))) {
+                E.syntax = s;
+
+                int filerow;
+                for(filerow = 0; filerow < E.numrows; filerow++) {
+                    editorUpdateSyntax(&E.row[filerow]);
+                }
+                return;
+            } 
+            i++;
+        }
     }
 }
 
@@ -411,6 +464,8 @@ void editorOpen(char *filename) {
     free(E.filename);
     E.filename = strdup(filename);
 
+    editorSelectSyntaxHighlight();
+
     FILE *fp = fopen(filename, "r");
     if (!fp) die("fopen");
 
@@ -436,6 +491,7 @@ void editorSave() {
             editorSetStatusMessage("Save aborted");
             return;
         }
+        editorSelectSyntaxHighlight();
     }
 
     int len;
@@ -581,7 +637,7 @@ void editorDrawsStatusBar(struct abuf *ab) {
     abAppend(ab, "\x1b[7m", 4);
     char status[80], rstatus[80];
     int len = snprintf(status, sizeof(status), " %.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? " (modified) " : "");
-    int rlen = snprintf(rstatus, sizeof(rstatus), " %d/%d ", E.cy+1, E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), " %s | %d/%d ", E.syntax ? E.syntax->filetype : "no ft", E.cy+1, E.numrows);
     if(len > E.screencols) len = E.screencols;
     abAppend(ab, status, len);
     while(len < E.screencols){
@@ -864,6 +920,7 @@ void initEditor() {
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
+    E.syntax = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
     E.screenrows -= 2;
