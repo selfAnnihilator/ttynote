6e34323d079d5067548d54ac3e69b36c208a6803
selfAnnihilator <159141717+selfAnnihilator@users.noreply.github.com>
2025-05-22 15:52:02 +0530

    set the number highlight to highlight only numbers and digits separated from text digits in identifier will not be highlighted

--- ttynote.c
+++ ttynote.c
@@ -196,14 +196,30 @@ int getWindowSize(int *rows, int *cols) {
 
 /**** syntax highlighting ****/
 
+int isSeperator(int c) {
+    return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
 void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    for(int i = 0; i < row->rsize; i++) {
-        if(isdigit(row->render[i])) {
+    int prevSep = 1;
+
+    int i = 0;
+    while (i < row->rsize) {
+        char c = row->render[i];
+        unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if((isdigit(c) && (prevSep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
             row->hl[i] = HL_NUMBER;
+            i++;
+            prevSep = 0;
+            continue;
         }
+
+        prevSep = isSeperator(c);
+        i++;
     }
 }
 
