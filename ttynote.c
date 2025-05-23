e133fbe5ff54c5738f6da24af3c07fbca14b8e42
selfAnnihilator <159141717+selfAnnihilator@users.noreply.github.com>
2025-05-23 08:22:55 +0530

    added single line comment syntax highlight

--- ttynote.c
+++ ttynote.c
@@ -41,6 +41,7 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
+    HL_COMMENT,
     HL_STRING,
     HL_NUMBER,
     HL_MATCH
@@ -55,6 +56,7 @@ enum editorHighlight {
 struct editorSyntax {
     char *filetype;
     char **filematch;
+    char *singleline_comment_start;
     int flags;
 };
 
@@ -94,6 +96,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
+        "//",
         HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
 };
@@ -232,6 +235,9 @@ void editorUpdateSyntax(erow *row) {
 
     if(E.syntax == NULL) return;
 
+    char *scs = E.syntax->singleline_comment_start;
+    int scs_len = scs ? strlen(scs) : 0;
+
     int prevSep = 1;
     int in_string = 0;
 
@@ -239,6 +245,13 @@ void editorUpdateSyntax(erow *row) {
     while (i < row->rsize) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if(scs_len && !in_string) {
+            if(!strncmp(&row->render[i], scs, scs_len)) {
+                memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+                break;
+            }
+        }        
 
         if(E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
             if(in_string) {
@@ -278,6 +291,7 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
     switch(hl) {
+        case HL_COMMENT: return 36;
         case HL_STRING: return 35;
         case HL_NUMBER: return 31;
         case HL_MATCH: return 34;
